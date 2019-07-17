57fbf9d5b328431984374c2d76f54481d921fb1b
Gregor <gregor@martynus.net>
2019-07-17 16:34:09 -0700

    feat: `.headers` and `.query` are no longer returned
    BREAKING CHANGE: `.headers` and `.query` are no longer returned

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { auth } from './auth'
-import { Token } from './types'
+import { auth } from "./auth";
+import { Token } from "./types";
 
-export function createTokenAuth (token: Token) {
-  return auth.bind(null, token)
+export function createTokenAuth(token: Token) {
+  if (!token) {
+    throw new Error("[@octokit/auth-token] No token passed to createTokenAuth");
+  }
+  if (typeof token !== "string") {
+    throw new Error(
+      "[@octokit/auth-token] Token passed to createTokenAuth is not a string"
+    );
+  }
+  return auth.bind(null, token);
 }
-

ec7756785d4bdae05528b9bc397775c68c93fa34
Gregor <gregor@martynus.net>
2019-07-17 16:34:09 -0700

    feat: `.headers` and `.query` are no longer returned
    BREAKING CHANGE: `.headers` and `.query` are no longer returned

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,11 @@
-import { Token } from './types'
+import { Token, Authentication } from "./types";
 
-export async function auth (token: Token) {
-  const tokenType = /^v\d+\./.test(token) ? 'installation' : 'oauth'
+export async function auth(token: Token): Promise<Authentication> {
+  const tokenType = /^v\d+\./.test(token) ? "installation" : "oauth";
 
   return {
-    type: 'token',
-    headers: {
-      authorization: `token ${token}`
-    },
+    type: "token",
     token: token,
-    tokenType,
-    query: {}
-  }
+    tokenType
+  };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,13 @@
-export type Token = string
+export type Token = string;
+
+export type OAuthTokenAuthentication = {
+  type: "token";
+  tokenType: "oauth";
+  token: Token;
+};
+export type InstallationTokenAuthentication = {
+  type: "token";
+  tokenType: "installation";
+  token: Token;
+};
+export type Authentication = BasicAuthentication | TokenAuthentication;

e3609a8089c6aca36e483c7d0a4a06d89463362f
Gregor <gregor@martynus.net>
2019-07-17 16:33:43 -0700

    test: `.headers` and `.query` are no longer returned

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,46 +1,56 @@
-import { createTokenAuth } from '../src/index'
+import { createTokenAuth } from "../src/index";
 
-test('README example', async () => {
-  const auth = createTokenAuth('1234567890abcdef1234567890abcdef12345678')
+test("README example", async () => {
+  const auth = createTokenAuth("1234567890abcdef1234567890abcdef12345678");
   const authentication = await auth();
 
   expect(authentication).toEqual({
-    type: 'token',
-    token: '1234567890abcdef1234567890abcdef12345678',
-    tokenType: 'oauth',
-    headers: {
-      authorization: 'token 1234567890abcdef1234567890abcdef12345678'
-    },
-    query: {}
-  })
-})
+    type: "token",
+    token: "1234567890abcdef1234567890abcdef12345678",
+    tokenType: "oauth"
+  });
+});
 
-test('installation token', async () => {
-  const auth = createTokenAuth('v1.1234567890abcdef1234567890abcdef12345678')
+test("installation token", async () => {
+  const auth = createTokenAuth("v1.1234567890abcdef1234567890abcdef12345678");
   const authentication = await auth();
 
   expect(authentication).toEqual({
-    type: 'token',
-    token: 'v1.1234567890abcdef1234567890abcdef12345678',
-    tokenType: 'installation',
-    headers: {
-      authorization: 'token v1.1234567890abcdef1234567890abcdef12345678'
-    },
-    query: {}
-  })
-})
+    type: "token",
+    token: "v1.1234567890abcdef1234567890abcdef12345678",
+    tokenType: "installation"
+  });
+});
 
-test('invalid token', async () => {
-  const auth = createTokenAuth('whatislove')
+test("invalid token", async () => {
+  const auth = createTokenAuth("whatislove");
   const authentication = await auth();
 
   expect(authentication).toEqual({
-    type: 'token',
-    token: 'whatislove',
-    tokenType: 'oauth',
-    headers: {
-      authorization: 'token whatislove'
-    },
-    query: {}
-  })
-})
+    type: "token",
+    token: "whatislove",
+    tokenType: "oauth"
+  });
+});
+
+test("no token", async () => {
+  try {
+    // @ts-ignore
+    const auth = createTokenAuth();
+    throw new Error("Should not resolve");
+  } catch (error) {
+    expect(error.message).toMatch(/no token passed to createTokenAuth/i);
+  }
+});
+
+test("token is not a string", async () => {
+  try {
+    // @ts-ignore
+    const auth = createTokenAuth({});
+    throw new Error("Should not resolve");
+  } catch (error) {
+    expect(error.message).toMatch(
+      /token passed to createTokenAuth is not a string/i
+    );
+  }
+});

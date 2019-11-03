e0466aced030a2b8cad32aceedc6e8f7167bf388
Gregor Martynus <gregor@martynus.net>
2019-11-02 17:50:21 -0700

    test: auth.hook() with bearer token

--- test/index.test.ts
+++ test/index.test.ts
@@ -72,34 +72,6 @@ test("token is not a string", async () => {
   }
 });
 
-test('auth.hook(request, "GET /user")', async () => {
-  const expectedRequestHeaders = {
-    accept: "application/vnd.github.v3+json",
-    authorization: "token 1234567890abcdef1234567890abcdef12345678",
-    "user-agent": "test"
-  };
-
-  const matchGetUser: MockMatcherFunction = (url, { body, headers }) => {
-    expect(url).toEqual("https://api.github.com/user");
-    expect(headers).toStrictEqual(expectedRequestHeaders);
-    return true;
-  };
-
-  const requestMock = request.defaults({
-    headers: {
-      "user-agent": "test"
-    },
-    request: {
-      fetch: fetchMock.sandbox().getOnce(matchGetUser, { id: 123 })
-    }
-  });
-
-  const { hook } = createTokenAuth("1234567890abcdef1234567890abcdef12345678");
-  const { data } = await hook(requestMock, "GET /user");
-
-  expect(data).toStrictEqual({ id: 123 });
-});
-
 test("OAuth token with prefix", async () => {
   const auth = createTokenAuth(
     "bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q"
@@ -152,4 +124,63 @@ test("JWT with capitalized prefix", async () => {
       "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q",
     tokenType: "app"
   });
+});
+
+test('auth.hook(request, "GET /user")', async () => {
+  const expectedRequestHeaders = {
+    accept: "application/vnd.github.v3+json",
+    authorization: "token 1234567890abcdef1234567890abcdef12345678",
+    "user-agent": "test"
+  };
+
+  const matchGetUser: MockMatcherFunction = (url, { body, headers }) => {
+    expect(url).toEqual("https://api.github.com/user");
+    expect(headers).toStrictEqual(expectedRequestHeaders);
+    return true;
+  };
+
+  const requestMock = request.defaults({
+    headers: {
+      "user-agent": "test"
+    },
+    request: {
+      fetch: fetchMock.sandbox().getOnce(matchGetUser, { id: 123 })
+    }
+  });
+
+  const { hook } = createTokenAuth("1234567890abcdef1234567890abcdef12345678");
+  const { data } = await hook(requestMock, "GET /user");
+
+  expect(data).toStrictEqual({ id: 123 });
+});
+
+test("auth.hook() with JWT", async () => {
+  const expectedRequestHeaders = {
+    accept: "application/vnd.github.v3+json",
+    authorization:
+      "bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q",
+    "user-agent": "test"
+  };
+
+  const matchGetUser: MockMatcherFunction = (url, { body, headers }) => {
+    expect(url).toEqual("https://api.github.com/user");
+    expect(headers).toStrictEqual(expectedRequestHeaders);
+    return true;
+  };
+
+  const requestMock = request.defaults({
+    headers: {
+      "user-agent": "test"
+    },
+    request: {
+      fetch: fetchMock.sandbox().getOnce(matchGetUser, { id: 123 })
+    }
+  });
+
+  const { hook } = createTokenAuth(
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q"
+  );
+  const { data } = await hook(requestMock, "GET /user");
+
+  expect(data).toStrictEqual({ id: 123 });
 });

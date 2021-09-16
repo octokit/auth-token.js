43a6d249982377fa7fa509ec9b137289fac956d8
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-09-16 10:27:08 -0700

    feat: support new token formats and `user-to-server` token type (#200)

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,23 +4,34 @@ import fetchMock, { MockMatcherFunction } from "fetch-mock";
 import { createTokenAuth } from "../src/index";
 
 test("README example", async () => {
-  const auth = createTokenAuth("1234567890abcdef1234567890abcdef12345678");
+  const auth = createTokenAuth("ghp_PersonalAccessToken01245678900000000");
   const authentication = await auth();
 
   expect(authentication).toEqual({
     type: "token",
-    token: "1234567890abcdef1234567890abcdef12345678",
+    token: "ghp_PersonalAccessToken01245678900000000",
     tokenType: "oauth",
   });
 });
 
-test("installation token", async () => {
+test("installation token (old format)", async () => {
   const auth = createTokenAuth("v1.1234567890abcdef1234567890abcdef12345678");
   const authentication = await auth();
 
   expect(authentication).toEqual({
     type: "token",
     token: "v1.1234567890abcdef1234567890abcdef12345678",
+    tokenType: "installation",
+  });
+});
+
+test("installation token (new format)", async () => {
+  const auth = createTokenAuth("ghs_InstallallationOrActionToken00000000");
+  const authentication = await auth();
+
+  expect(authentication).toEqual({
+    type: "token",
+    token: "ghs_InstallallationOrActionToken00000000",
     tokenType: "installation",
   });
 });
@@ -36,6 +47,17 @@ test("JSON Web Token (GitHub App Authentication)", async () => {
     token:
       "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q",
     tokenType: "app",
+  });
+});
+
+test("User-to-server token (User authentication through app installation)", async () => {
+  const auth = createTokenAuth("ghu_InstallationUserToServer000000000000");
+  const authentication = await auth();
+
+  expect(authentication).toEqual({
+    type: "token",
+    token: "ghu_InstallationUserToServer000000000000",
+    tokenType: "user-to-server",
   });
 });
 
@@ -88,26 +110,26 @@ test("OAuth token with prefix", async () => {
 
 test("JWT with prefix", async () => {
   const auth = createTokenAuth(
-    "token 1234567890abcdef1234567890abcdef12345678"
+    "token ghp_PersonalAccessToken01245678900000000"
   );
   const authentication = await auth();
 
   expect(authentication).toEqual({
     type: "token",
-    token: "1234567890abcdef1234567890abcdef12345678",
+    token: "ghp_PersonalAccessToken01245678900000000",
     tokenType: "oauth",
   });
 });
 
 test("JWT with capitalized prefix", async () => {
   const auth = createTokenAuth(
-    "Token 1234567890abcdef1234567890abcdef12345678"
+    "Token ghp_PersonalAccessToken01245678900000000"
   );
   const authentication = await auth();
 
   expect(authentication).toEqual({
     type: "token",
-    token: "1234567890abcdef1234567890abcdef12345678",
+    token: "ghp_PersonalAccessToken01245678900000000",
     tokenType: "oauth",
   });
 });
@@ -129,7 +151,7 @@ test("JWT with capitalized prefix", async () => {
 test('auth.hook(request, "GET /user")', async () => {
   const expectedRequestHeaders = {
     accept: "application/vnd.github.v3+json",
-    authorization: "token 1234567890abcdef1234567890abcdef12345678",
+    authorization: "token ghp_PersonalAccessToken01245678900000000",
     "user-agent": "test",
   };
 
@@ -148,7 +170,7 @@ test('auth.hook(request, "GET /user")', async () => {
     },
   });
 
-  const { hook } = createTokenAuth("1234567890abcdef1234567890abcdef12345678");
+  const { hook } = createTokenAuth("ghp_PersonalAccessToken01245678900000000");
   const { data } = await hook(requestMock, "GET /user");
 
   expect(data).toStrictEqual({ id: 123 });

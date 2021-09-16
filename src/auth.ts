6863739622e6df544457895be4fcae9e157585f0
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-09-16 10:22:06 -0700

    feat: support new token formats and `user-to-server` token type

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,23 @@
 import { Token, Authentication } from "./types";
 
+const REGEX_IS_INSTALLATION_LEGACY = /^v1\./;
+const REGEX_IS_INSTALLATION = /^ghs_/;
+const REGEX_IS_USER_TO_SERVER = /^ghu_/;
+
 export async function auth(token: Token): Promise<Authentication> {
-  const tokenType =
-    token.split(/\./).length === 3
-      ? "app"
-      : /^v\d+\./.test(token)
-      ? "installation"
-      : "oauth";
+  const isApp = token.split(/\./).length === 3;
+  const isInstallation =
+    REGEX_IS_INSTALLATION_LEGACY.test(token) ||
+    REGEX_IS_INSTALLATION.test(token);
+  const isUserToServer = REGEX_IS_USER_TO_SERVER.test(token);
+
+  const tokenType = isApp
+    ? "app"
+    : isInstallation
+    ? "installation"
+    : isUserToServer
+    ? "user-to-server"
+    : "oauth";
 
   return {
     type: "token",

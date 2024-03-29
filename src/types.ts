81c4d7307d368029cd753b5b59c87a33c051ebc9
Nathan Gendron <nato@coderabbit.ai>
2024-03-28 23:19:34 -0400

    📝 Add documentation for token types

--- src/types.ts
+++ src/types.ts
@@ -12,11 +12,34 @@ export type RequestParameters = OctokitTypes.RequestParameters;
 export type RequestInterface = OctokitTypes.RequestInterface;
 export type Route = OctokitTypes.Route;
 
+/** GitHub App server-to-server token
+ * @see https://docs.github.com/en/developers/apps/authenticating-with-github-apps#authenticating-as-an-installation */
 export type ActionToken = `ghs_${string}`;
+/** GitHub App server-to-server token
+ * @see https://docs.github.com/en/developers/apps/authenticating-with-github-apps#authenticating-as-an-installation */
 export type InstallallationToken = `ghs_${string}`;
 export type InstallallationTokenV1 = `v1.${string}`;
+/** Your app can make API requests on behalf of a user. API requests made by an
+ * app on behalf of a user will be attributed to that user. For example, if your
+ * app posts a comment on behalf of a user, the GitHub UI will show the user's
+ * avatar photo along with the app's identicon badge as the author of the issue.
+ *
+ * @see https://docs.github.com/en/developers/apps/identifying-and-authorizing-users-for-github-apps
+ * */
 export type InstallationUserToServerToken = `ghu_${string}`;
-export type OAuthToken = `${string}.${string}.${string}`;
+/** In order to authenticate as an app or generate an installation access token,
+ * you must generate a JSON Web Token (JWT). If a REST API endpoint requires a
+ * JWT, the documentation for that endpoint will indicate that you must use a
+ * JWT to access the endpoint.
+ *
+ * @see https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app
+ */
+export type JWT = `${string}.${string}.${string}`;
+/** This access token allows you to make requests to the API on a behalf of a user.
+ *
+ * @see https://docs.github.com/en/developers/apps/authorizing-oauth-apps
+ */
+export type OAuthToken = `gho_${string}`;
 /**
  * Personal access tokens are an alternative to using passwords for
  * authentication to GitHub when using the [GitHub API](https://docs.github.com/en/rest/overview/authenticating-to-the-rest-api)
@@ -27,12 +50,13 @@ export type OAuthToken = `${string}.${string}.${string}`;
 export type PersonalAccessToken = `ghp_${string}`;
 
 export type Token =
-  | PersonalAccessToken
-  | InstallallationTokenV1
-  | InstallallationToken
   | ActionToken
+  | InstallallationToken
+  | InstallallationTokenV1
   | InstallationUserToServerToken
-  | OAuthToken;
+  | JWT
+  | OAuthToken
+  | PersonalAccessToken;
 
 export type BearerToken =
   | `bearer ${Token}`

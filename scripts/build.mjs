95a5ec0160c358b5b24bb18fe770f76f1edbe783
wolfy1339 <webmaster@wolfy1339.com>
2024-04-15 12:34:18 -0400

    fix(pkg): add `default` fallback and `types` export

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -61,13 +61,16 @@ async function main() {
       {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
-        main: "./dist-bundle/index.js",
         types: "./dist-types/index.d.ts",
         exports: {
           ".": {
             types: "./dist-types/index.d.ts",
             import: "./dist-bundle/index.js",
+            default: "./dist-bundle/index.js",
           },
+          "./types": {
+            types: "./dist-types/index.d.ts",
+          }
         },
         sideEffects: false,
       },
